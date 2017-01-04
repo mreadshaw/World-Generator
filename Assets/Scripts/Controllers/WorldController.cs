5b376bd6e4e839e24b66cad9739187f2350b2086
mreadshaw <matt@Matts-MacBook-Pro.local>
2017-01-03 17:11:48 -0800

    add world class

---
+++ Assets/Scripts/Controllers/WorldController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+	World world;
+
+	// Use this for initialization
+	void Start () {
+		world = new World ();
+		Debug.Log("World created.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
